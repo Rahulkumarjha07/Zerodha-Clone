4c8eb5f5fbcb6750bd3bd914c43e9b92b6bdc5f3
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-03-22 19:35:24 +0530

    hero section of home completed

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,7 +1,21 @@
-export default function hero(){
+import Button from '@mui/material/Button';
+
+export default function Hero(){
     return(
-        <h1>hero</h1>
+        <div className='container p-5'>
+            <div className="row text-center">
+                <img src='media/homeHero.png' alt='trading dashboard' className='mb-5' />
+
+                <h1 className='mt-2'>Invest in everything</h1>
+
+                <p>Online platform to invest, derivatives, mutual funds, stocks and more</p>
+
+               <div className="d-flex justify-content-center">
+                  <Button variant="contained" size="medium">
+                      Sign Up
+                    </Button>
+                </div>
+            </div>
+        </div>
     );
 }
-
-    
