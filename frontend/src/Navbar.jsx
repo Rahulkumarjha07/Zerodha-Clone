22df2d24baf9671421eaecab3344cc906edbe9b4
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-03-23 11:49:49 +0530

    navbar and openaccount in home added

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,7 +1,61 @@
-export default function Navbar(){
-    return(
-        <h1>Navbar</h1>
-    );
-}
+export default function Navbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div className="container p-1">
 
-    
+        {/* Logo */}
+        <a className="navbar-brand" href="#">
+          <img
+            src="/media/logo.svg"
+            alt="logo"
+            style={{ width: "120px" }}
+          />
+        </a>
+
+        {/* Toggle Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Right Side Menu */}
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav">
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Signup</a>
+            </li>
+
+            <li className="nav-item ">
+              <a className="nav-link active" href="#">About</a>
+            </li>
+
+            <li className="nav-item ">
+              <a className="nav-link active" href="#">Products</a>
+            </li>
+
+            <li className="nav-item ">
+              <a className="nav-link active" href="#">Pricing</a>
+            </li>
+
+            <li className="nav-item active">
+              <a className="nav-link active" href="#">Support</a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  );
+}
--- frontend/src/Openaccount.js
+++ frontend/src/Openaccount.js
@@ -1,7 +1,24 @@
-export default function Openaccount(){
-    return(
-        <div>
-            <h1>Openaccount</h1>
+import Button from "@mui/material/Button";
+
+export default function Openaccount() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+
+
+        <h1 className="mt-2 fs-2">Open a Zerodha account</h1>
+
+        <p className="text-muted">
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades
+        </p>
+
+        <div className="d-flex justify-content-center mt-3">
+          <Button variant="contained" size="medium">
+            Sign up now
+          </Button>
         </div>
-    );
+
+      </div>
+    </div>
+  );
 }
