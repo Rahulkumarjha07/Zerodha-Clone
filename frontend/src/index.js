edef48a1fa47a7384cab4e16a46d5c6c3ae1029d
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-03-24 13:28:44 +0530

    routes configured succesfully

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import HomePage from "./landing_page/home/HomePage";
-
-
+import Signup from "./landing_page/signup/Signup.jsx";
+import AboutPage from "./landing_page/about/Aboutpage.jsx";
+import  ProductPage from "./landing_page/products/ProductPage.jsx";
+import PricingPage from "./landing_page/pricing/Pricingpage.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-  
-    <HomePage />
-   
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} /> {/* ✅ Added route */}
+       <Route path="/about" element={<AboutPage />} />
+       <Route path="/product" element={<ProductPage/>} />
+        <Route path="/pricing" element={<PricingPage />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-
