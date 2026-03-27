56811e7475a985a04e0e0b7973807372cd5352c6
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-03-27 20:49:37 +0530

    postions and holdings configured

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,94 +1,73 @@
-import React, { useState, useEffect } from "react";
-import axios, { all } from "axios";
+import React from "react";
+import { holdings } from "../data/data.js";
 import { VerticalGraph } from "./VerticalGraph";
 
-// import { holdings } from "../data/data";
-
 const Holdings = () => {
-  const [allHoldings, setAllHoldings] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
-      // console.log(res.data);
-      setAllHoldings(res.data);
-    });
-  }, []);
-
-  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  const labels = allHoldings.map((subArray) => subArray["name"]);
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Stock Price",
-        data: allHoldings.map((stock) => stock.price),
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
-
   
+
   return (
     <>
-      <h3 className="title">Holdings ({allHoldings.length})</h3>
+      <h3 className="title">Holdings ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
-          <tr>
-            <th>Instrument</th>
-            <th>Qty.</th>
-            <th>Avg. cost</th>
-            <th>LTP</th>
-            <th>Cur. val</th>
-            <th>P&L</th>
-            <th>Net chg.</th>
-            <th>Day chg.</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg. cost</th>
+              <th>LTP</th>
+              <th>Cur. val</th>
+              <th>P&L</th>
+              <th>Net chg.</th>
+              <th>Day chg.</th>
+            </tr>
+          </thead>
 
-          {allHoldings.map((stock, index) => {
-            const curValue = stock.price * stock.qty;
-            const isProfit = curValue - stock.avg * stock.qty >= 0.0;
-            const profClass = isProfit ? "profit" : "loss";
-            const dayClass = stock.isLoss ? "loss" : "profit";
+          <tbody>
+            {holdings.map((stock, index) => {
+              const currval = stock.price * stock.qty;
+              const profit = currval - stock.avg * stock.qty;
 
-            return (
-              <tr key={index}>
-                <td>{stock.name}</td>
-                <td>{stock.qty}</td>
-                <td>{stock.avg.toFixed(2)}</td>
-                <td>{stock.price.toFixed(2)}</td>
-                <td>{curValue.toFixed(2)}</td>
-                <td className={profClass}>
-                  {(curValue - stock.avg * stock.qty).toFixed(2)}
-                </td>
-                <td className={profClass}>{stock.net}</td>
-                <td className={dayClass}>{stock.day}</td>
-              </tr>
-            );
-          })}
+              const profclass = profit >= 0 ? "profit" : "loss";
+              const dayClass = stock.day.includes("-") ? "loss" : "profit";
+
+              return (
+                <tr key={index}>
+                  <td>{stock.name}</td>
+                  <td>{stock.qty}</td>
+                  <td>{stock.avg.toFixed(2)}</td>
+                  <td>{stock.price.toFixed(2)}</td>
+                  <td>{currval.toFixed(2)}</td>
+                  <td className={profclass}>{profit.toFixed(2)}</td>
+                  <td className={profclass}>{stock.net}</td>
+                  <td className={dayClass}>{stock.day}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
 
+      {/* Static Summary */}
       <div className="row">
         <div className="col">
-          <h5>
-            29,875.<span>55</span>{" "}
-          </h5>
-          <p>Total investment</p>
+          <h5>5000.00</h5>
+          <p>Total Investment</p>
         </div>
+
         <div className="col">
-          <h5>
-            31,428.<span>95</span>{" "}
-          </h5>
-          <p>Current value</p>
+          <h5>6000.00</h5>
+          <p>Current Value</p>
         </div>
+
         <div className="col">
-          <h5>1,553.40 (+5.20%)</h5>
-          <p>P&L</p>
+          <h5 className="profit">1000.00 (20.00%)</h5>
+          <p>P&amp;L</p>
         </div>
       </div>
-      <VerticalGraph data={data} />
+
+      
     </>
   );
 };
