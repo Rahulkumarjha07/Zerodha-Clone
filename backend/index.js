c1600746b33f80507ed6cb5b6400b245a2845eff
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-03-28 11:56:32 +0530

    db base configured

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 
+const mongoose = require("mongoose");
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
+const URL = process.env.MONGO_URL;
+
+console.log("Mongo URL:", URL);
+
+mongoose.connect(URL).then(()=>{
+    console.log("db is connceted");
+})
+.catch(()=>{
+    console.log("db connection failed");
+});
 app.get("/",(req,res)=>{
     res.send("root user");
 });
 
-app.listen(8080,()=>{
+app.listen(PORT,()=>{
  console.log("app is listening on the port 8080");
 });
